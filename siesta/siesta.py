08e0341402f08562333d09c06094e7721752a9a0
Neron-png <neronmkp@csd.auth.gr>
2026-04-23 18:43:00 +0300

    Packaged to library, run python3 -m build

--- siesta/siesta.py
+++ siesta/siesta.py
@@ -15,7 +15,7 @@ Example usage:
     python3 main.py --config config/preprocess.config.json preprocess
 '''
 
-import sys;from pathlib import Path;sys.path.insert(0, str(Path(__file__).parent.parent))
+#import sys;from pathlib import Path;sys.path.insert(0, str(Path(__file__).parent.parent))
 from siesta.core.app import Siesta
 from siesta.api import router
 import argparse
