86984299171acc1bca8f93379a31272342f1e2e1
Christos Balaktsis <balaktsis@csd.auth.gr>
2026-04-26 03:48:19 +0300

    Update key handling in S3Manager for uploads
    Modify key handling to append timestamp and change key extraction logic.

--- siesta/storage/S3/S3Manager.py
+++ siesta/storage/S3/S3Manager.py
@@ -13,6 +13,7 @@ from siesta.model.DataModel import Event, EventConfig, Last_Checked_table_schema
 from siesta.core.config import get_system_config
 import siesta.core.sparkManager as SparkManager
 from delta.tables import DeltaTable
+from datetime import datetime
 
 import logging
 logger = logging.getLogger(__name__)
@@ -323,8 +324,9 @@ class S3Manager(StorageManager):
         """
         
         key = destination_path
-        if key.startswith("/"):
-            key = key[1:]
+        if key.contains("/"):
+            key = key.split("/")[-1]
+        key += f".{int(datetime.now().timestamp())}"
         key = f"{preprocess_config.get('log_name', 'default_log')}/batches/{key}"
             
         logger.info(f"Uploading '{local_path}' to bucket '{preprocess_config.get('storage_namespace', 'siesta')}' with key '{key}'...")
